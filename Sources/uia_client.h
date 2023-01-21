f4078b63b181ba6188ee3b8cf1d642b52b554a66
bijanbina <bijanbina@gmail.com>
2023-01-21 08:54:22 +0100

    Add UIA Pattern ID

--- Sources/uia_client.h
+++ Sources/uia_client.h
@@ -108,22 +108,102 @@ typedef interface IUIAutomationProxyFactoryMapping IUIAutomationProxyFactoryMapp
 typedef void *UIA_HWND;
 
 enum TreeScope
-    {
-        TreeScope_None	= 0,
-        TreeScope_Element	= 0x1,
-        TreeScope_Children	= 0x2,
-        TreeScope_Descendants	= 0x4,
-        TreeScope_Parent	= 0x8,
-        TreeScope_Ancestors	= 0x10,
-        TreeScope_Subtree	= ( ( TreeScope_Element | TreeScope_Children )  | TreeScope_Descendants )
-    } ;
+{
+    TreeScope_None	= 0,
+    TreeScope_Element	= 0x1,
+    TreeScope_Children	= 0x2,
+    TreeScope_Descendants	= 0x4,
+    TreeScope_Parent	= 0x8,
+    TreeScope_Ancestors	= 0x10,
+    TreeScope_Subtree	= ( ( TreeScope_Element | TreeScope_Children )  | TreeScope_Descendants )
+} ;
 
 enum PropertyConditionFlags
-    {
-        PropertyConditionFlags_None	= 0,
-        PropertyConditionFlags_IgnoreCase	= 0x1,
-        PropertyConditionFlags_MatchSubstring	= 0x2
-    } ;
+{
+    PropertyConditionFlags_None	= 0,
+    PropertyConditionFlags_IgnoreCase	= 0x1,
+    PropertyConditionFlags_MatchSubstring	= 0x2
+} ;
+
+#ifndef __UIA_PatternIds_MODULE_DEFINED__
+#define __UIA_PatternIds_MODULE_DEFINED__
+
+
+/* module UIA_PatternIds */
+/* [dllname] */
+
+const long UIA_InvokePatternId	=	10000;
+
+const long UIA_SelectionPatternId	=	10001;
+
+const long UIA_ValuePatternId	=	10002;
+
+const long UIA_RangeValuePatternId	=	10003;
+
+const long UIA_ScrollPatternId	=	10004;
+
+const long UIA_ExpandCollapsePatternId	=	10005;
+
+const long UIA_GridPatternId	=	10006;
+
+const long UIA_GridItemPatternId	=	10007;
+
+const long UIA_MultipleViewPatternId	=	10008;
+
+const long UIA_WindowPatternId	=	10009;
+
+const long UIA_SelectionItemPatternId	=	10010;
+
+const long UIA_DockPatternId	=	10011;
+
+const long UIA_TablePatternId	=	10012;
+
+const long UIA_TableItemPatternId	=	10013;
+
+const long UIA_TextPatternId	=	10014;
+
+const long UIA_TogglePatternId	=	10015;
+
+const long UIA_TransformPatternId	=	10016;
+
+const long UIA_ScrollItemPatternId	=	10017;
+
+const long UIA_LegacyIAccessiblePatternId	=	10018;
+
+const long UIA_ItemContainerPatternId	=	10019;
+
+const long UIA_VirtualizedItemPatternId	=	10020;
+
+const long UIA_SynchronizedInputPatternId	=	10021;
+
+const long UIA_ObjectModelPatternId	=	10022;
+
+const long UIA_AnnotationPatternId	=	10023;
+
+const long UIA_TextPattern2Id	=	10024;
+
+const long UIA_StylesPatternId	=	10025;
+
+const long UIA_SpreadsheetPatternId	=	10026;
+
+const long UIA_SpreadsheetItemPatternId	=	10027;
+
+const long UIA_TransformPattern2Id	=	10028;
+
+const long UIA_TextChildPatternId	=	10029;
+
+const long UIA_DragPatternId	=	10030;
+
+const long UIA_DropTargetPatternId	=	10031;
+
+const long UIA_TextEditPatternId	=	10032;
+
+const long UIA_CustomNavigationPatternId	=	10033;
+
+const long UIA_SelectionPattern2Id	=	10034;
+
+#endif /* __UIA_PatternIds_MODULE_DEFINED__ */
+
 
 #endif // MSVC Compiler
 
