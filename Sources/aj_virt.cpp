bc1bf8b6d6c92357d92f5eb3ab28d326d14f3930
bijanbina <bijanbina@gmail.com>
2023-11-24 17:45:38 +0330

    Update AjVirt to Support Windows 11 21H2[E&B]

--- Sources/aj_virt.cpp
+++ Sources/aj_virt.cpp
@@ -3,27 +3,33 @@
 #include <windows.h>
 #include <QDebug>
 #include <QThread>
+#include "mm_win32_guid.h"
 
 AjVirt::AjVirt(QObject *parent): QObject(parent)
 {
-    pDesktopManagerInt = NULL;
+    manager_int = NULL;
     CoInitializeEx(NULL, COINIT_MULTITHREADED);
 
-    IServiceProvider* pServiceProvider = NULL;
+    IServiceProvider* services = NULL;
     HRESULT hr = CoCreateInstance(
                         CLSID_ImmersiveShell, NULL, CLSCTX_LOCAL_SERVER,
-                        __uuidof(IServiceProvider), (PVOID*)&pServiceProvider); 
+                        __uuidof(IServiceProvider), (PVOID*)&services);
 
-    hr = pServiceProvider->QueryService(CLSID_VirtualDesktopManager,
+    hr = services->QueryService(CLSID_VirtualDesktopManager,
                                         IID_IVirtualDesktopManager,
                                         (void **)&pDesktopManager);
 
-    hr = pServiceProvider->QueryService(CLSID_VirtualDesktopAPI_Unknown,
-                                        IID_IVirtualDesktopManagerInternal,
-                                        (void **)&pDesktopManagerInt);
-    pServiceProvider->Release();
+    win_ver = mm_getWinVer();
+    if( win_ver==MM_WIN10 )
+    {
+        initInternal_Win10(services);
+    }
+    else if( win_ver==MM_WIN11_21H2 )
+    {
+        initInternal_Win11_21H2(services);
+    }
 
-    updateGUID();
+    services->Release();
 
     current_desktop = getCurrDesktop();
     last_desktop = -1;
@@ -31,23 +37,40 @@ AjVirt::AjVirt(QObject *parent): QObject(parent)
 
 AjVirt::~AjVirt()
 {
-    pDesktopManagerInt->Release();
+    if( win_ver==MM_WIN10 )
+    {
+        manager_int->Release();
+    }
+    else if( win_ver==MM_WIN11_21H2 )
+    {
+        manager_int_win11_21H2->Release();
+    }
 }
 
-void AjVirt::updateGUID()
+void AjVirt::initInternal_Win10(IServiceProvider* service)
 {
-    IObjectArray *desktops;
+    IObjectArray *desktops = NULL;
     IVirtualDesktop *c_desktop;
+    HRESULT hr;
 
-    pDesktopManagerInt->GetDesktops(&desktops);
+    hr = service->QueryService(CLSID_VirtualDesktopAPI_Unknown,
+                               IID_IVirtualDesktopManagerInternal_Win10,
+                               (void **)&manager_int);
+    if( hr )
+    {
+        qDebug() << "IVirtualDesktopManagerInternal Failed" << hr;
+    }
+
+    hr = manager_int->GetDesktops(&desktops);
     UINT count;
     desktops->GetCount(&count);
 
+    GUID buffer;
     for( unsigned int i=0 ; i<count ; i++ )
     {
-        desktops->GetAt(i, UUID_IVirtualDesktop, (void**)&c_desktop);
+        desktops->GetAt(i, UUID_IVirtualDesktop_Win10,
+                        (void**)&c_desktop);
 
-        GUID buffer;
         c_desktop->GetID(&buffer);
         vd_guids << buffer;
         vd_desks << c_desktop;
@@ -56,21 +79,73 @@ void AjVirt::updateGUID()
     desktops->Release();
 }
 
+void AjVirt::initInternal_Win11_21H2(IServiceProvider* service)
+{
+    IObjectArray *desktops = NULL;
+    IVirtualDesktop_Win11_21H2 *c_desktop_win11_21H2;
+    HRESULT hr;
+
+    hr = service->QueryService(CLSID_VirtualDesktopAPI_Unknown,
+                        IID_IVirtualDesktopManagerInternal_Win11_21H2,
+                        (void **)&manager_int_win11_21H2);
+    if( hr )
+    {
+        qDebug() << "IVirtualDesktopManagerInternal Failed" << hr;
+    }
+
+    hr = manager_int_win11_21H2->GetDesktops(NULL, &desktops);
+    UINT count;
+    desktops->GetCount(&count);
+
+    GUID buffer;
+
+    for( unsigned int i=0 ; i<count ; i++ )
+    {
+        desktops->GetAt(i, UUID_IVirtualDesktop_Win11_21H2,
+                        (void**)&c_desktop_win11_21H2);
+
+
+        c_desktop_win11_21H2->GetID(&buffer);
+        vd_guids << buffer;
+        vd_desks_win11_21H2 << c_desktop_win11_21H2;
+    }
+
+    desktops->Release();
+}
+
 void AjVirt::setDesktop(int id)
 {
-    if( id<vd_desks.length() && id>=0 )
+    if( win_ver==MM_WIN10 )
     {
-        current_desktop = getCurrDesktop();
-        if( current_desktop==id+1 )
+        if( id>vd_desks.length()-1 || id<0 )
         {
             return;
         }
-        int res = pDesktopManagerInt->SwitchDesktop(vd_desks[id]);
-//        qDebug() << "setDesktop" << res;
-        setFocus();
-        last_desktop = current_desktop;
-        current_desktop = id + 1;
     }
+    else if( win_ver==MM_WIN11_21H2 )
+    {
+        if( id>vd_desks_win11_21H2.length()-1 || id<0 )
+        {
+            return;
+        }
+    }
+    current_desktop = getCurrDesktop();
+    if( current_desktop==id+1 )
+    {
+        return;
+    }
+    if( win_ver==MM_WIN10 )
+    {
+        manager_int->SwitchDesktop(vd_desks[id]);
+    }
+    else if( win_ver==MM_WIN11_21H2 )
+    {
+        manager_int_win11_21H2->SwitchDesktop(NULL,
+                                              vd_desks_win11_21H2[id]);
+    }
+//        qDebug() << "setDesktop" << res;
+    last_desktop = current_desktop;
+    current_desktop = id + 1;
 }
 
 int AjVirt::isCurrentActive()
@@ -92,23 +167,55 @@ void AjVirt::setFocus()
 
 int AjVirt::getCurrDesktop()
 {
-    IVirtualDesktop *currDesktop;
-
-    pDesktopManagerInt->GetCurrentDesktop(&currDesktop);
-    GUID curr_DesktopGUID;
-    currDesktop->GetID(&curr_DesktopGUID);
-    currDesktop->Release();
+    GUID curr_desktop_GUID = getCurrDesktopGuid();
 
     for( int i=0 ; i<vd_guids.length() ; i++ )
     {
-        if( curr_DesktopGUID==vd_guids[i] )
+        if( curr_desktop_GUID==vd_guids[i] )
         {
-            return i+1;
+            current_desktop = i+1;
+            return current_desktop;
         }
     }
 
-    return -1;
+    return 1;
 }
+
+GUID AjVirt::getCurrDesktopGuid()
+{
+    HRESULT hr;
+    GUID curr_desktop_GUID;
+
+    if( win_ver==MM_WIN10 )
+    {
+        IVirtualDesktop *currDesktop;
+
+        hr = manager_int->GetCurrentDesktop(&currDesktop);
+
+        if( currDesktop==NULL )
+        {
+            qDebug() << "Error 21: Mom is not movable!";
+        }
+        currDesktop->GetID(&curr_desktop_GUID);
+        currDesktop->Release();
+    }
+    else if( win_ver==MM_WIN11_21H2 )
+    {
+        IVirtualDesktop_Win11_21H2 *currDesktop;
+
+        hr = manager_int_win11_21H2->GetCurrentDesktop(NULL,
+                                                       &currDesktop);
+
+        if( currDesktop==NULL )
+        {
+            qDebug() << "Error 21: Mom is not movable!";
+        }
+        currDesktop->GetID(&curr_desktop_GUID);
+        currDesktop->Release();
+    }
+    return curr_desktop_GUID;
+}
+
 
 void AjVirt::sendKey(int key_val)
 {
--- Sources/aj_virt.h
+++ Sources/aj_virt.h
@@ -7,6 +7,7 @@
 #include <QObject>
 #include <QTimer>
 #include <windows.h>
+#include <Hstring.h>
 #include <inspectable.h> // IInspectable, HSTRING, TrustLevel
 #include <shobjidl.h> // IObjectArray, ObjectArray, IVirtualDesktopManager, VirtualDesktopManager
 #include <strsafe.h> // StringCbPrintf
@@ -21,15 +22,9 @@ const CLSID CLSID_ImmersiveShell = {
 const CLSID CLSID_VirtualDesktopAPI_Unknown = {
     0xC5E0CDCA, 0x7B6E, 0x41B2, {0x9F, 0xC4, 0xD9, 0x39, 0x75, 0xCC, 0x46, 0x7B} };
 
-const GUID UUID_IVirtualDesktop = {
-    0xFF72FFDD, 0xBE7E, 0x43FC, {0x9C, 0x03, 0xAD, 0x81, 0x68, 0x1E, 0x88, 0xE4} };
-
-const IID IID_IVirtualDesktopManagerInternal = {
-    0xf31574d6, 0xb682, 0x4cdc, {0xbd, 0x56, 0x18, 0x27, 0x86, 0x0a, 0xbe, 0xc6} };
 #ifndef _MSC_VER
 const CLSID CLSID_VirtualDesktopManager = {
     0xa5cd92ff, 0x29be, 0x454c, {0x8d, 0x04, 0xd8, 0x28, 0x79, 0xfb, 0x3f, 0x1b} };
-
 
 const IID IID_IVirtualDesktopManager = {
     0xA5CD92FF, 0x29BE, 0x454C, {0x8D, 0x04, 0xD8, 0x28, 0x79, 0xFB, 0x3F, 0x1B} };
@@ -47,6 +42,25 @@ public:
 
     virtual HRESULT STDMETHODCALLTYPE GetID(
         GUID *pGuid) = 0;
+};
+
+struct IVirtualDesktop_Win11_21H2 : public IUnknown
+{
+public:
+    virtual HRESULT STDMETHODCALLTYPE IsViewVisible(
+        IApplicationView *pView, int *pfVisible) = 0;
+
+    virtual HRESULT STDMETHODCALLTYPE GetID(
+        GUID *pGuid) = 0;
+
+    virtual HRESULT STDMETHODCALLTYPE GetMonitor(
+        HMONITOR hMonitor) = 0;
+
+    virtual HRESULT STDMETHODCALLTYPE GetName(
+        HSTRING* pName) = 0;
+
+    virtual HRESULT STDMETHODCALLTYPE GetWallpaper(
+        HSTRING* pName) = 0;
 };
 
 struct IVirtualDesktopManagerInternal : public IUnknown
@@ -93,6 +107,78 @@ public:
         IVirtualDesktop **ppDesktop) = 0;
 };
 
+struct IVirtualDesktopManagerInternal_WIN11_21H2 : public IUnknown
+{
+public:
+    virtual HRESULT STDMETHODCALLTYPE GetCount(HMONITOR hMonitor,
+        UINT *pCount) = 0;
+
+    virtual HRESULT STDMETHODCALLTYPE MoveViewToDesktop(
+        IApplicationView *pView,
+        IVirtualDesktop_Win11_21H2 *pDesktop) = 0;
+
+    // 10240
+    virtual HRESULT STDMETHODCALLTYPE CanViewMoveDesktops(
+        IApplicationView *pView,
+        BOOL *pfCanViewMoveDesktops) = 0;
+
+    virtual HRESULT STDMETHODCALLTYPE GetCurrentDesktop(
+        HMONITOR hMonitor,
+        IVirtualDesktop_Win11_21H2** desktop) = 0;
+
+    virtual HRESULT STDMETHODCALLTYPE GetDesktops(
+        HMONITOR hMonitor,
+        IObjectArray **ppDesktops) = 0;
+
+    virtual HRESULT STDMETHODCALLTYPE GetAdjacentDesktop(
+        IVirtualDesktop_Win11_21H2 *pDesktopReference,
+        int uDirection,
+        IVirtualDesktop_Win11_21H2 **ppAdjacentDesktop) = 0;
+
+    virtual HRESULT STDMETHODCALLTYPE SwitchDesktop(
+        HMONITOR hMonitor,
+        IVirtualDesktop_Win11_21H2 *pDesktop) = 0;
+
+    virtual HRESULT STDMETHODCALLTYPE CreateDesktopW(
+        IVirtualDesktop_Win11_21H2 **ppNewDesktop) = 0;
+
+    // 21313
+    virtual HRESULT STDMETHODCALLTYPE MoveDesktop(
+            IVirtualDesktop_Win11_21H2* pMoveDesktop, HMONITOR hMonitor, int nIndex) = 0;
+
+    virtual HRESULT STDMETHODCALLTYPE RemoveDesktop(
+        IVirtualDesktop_Win11_21H2 *pRemove,
+        IVirtualDesktop_Win11_21H2 *pFallbackDesktop) = 0;
+
+    // 10240
+    virtual HRESULT STDMETHODCALLTYPE FindDesktop(
+        GUID *desktopId,
+        IVirtualDesktop_Win11_21H2 **ppDesktop) = 0;
+
+    virtual HRESULT STDMETHODCALLTYPE GetDesktopSwitchIncludeExcludeViews(
+            IVirtualDesktop_Win11_21H2* pDesktop, IObjectArray** ppLeftDesktops,
+            IObjectArray** ppRightDesktops) = 0;
+
+    virtual HRESULT STDMETHODCALLTYPE SetDesktopName(
+            IVirtualDesktop_Win11_21H2* pDesktop, HSTRING name) = 0;
+
+    virtual HRESULT STDMETHODCALLTYPE SetDesktopWallpaper(
+            IVirtualDesktop_Win11_21H2* pDesktop, HSTRING path) = 0;
+
+    virtual HRESULT STDMETHODCALLTYPE UpdateWallpaperPathForAllDesktops(
+            HSTRING text) = 0;
+
+    virtual HRESULT STDMETHODCALLTYPE CopyDesktopState(
+            /*IApplicationView*/ IUnknown* pView0,
+            /*IApplicationView*/ IUnknown* pView1) = 0;
+
+    virtual HRESULT STDMETHODCALLTYPE GetDesktopIsPerMonitor(
+            BOOL* bPerMonitor) = 0;
+
+    // 21359
+    virtual HRESULT STDMETHODCALLTYPE SetDesktopIsPerMonitor(BOOL state) = 0;
+};
+
 #ifndef _MSC_VER
 MIDL_INTERFACE("a5cd92ff-29be-454c-8d04-d82879fb3f1b")
 IVirtualDesktopManager : public IUnknown
@@ -133,16 +219,20 @@ public slots:
     void setDesktop(int id);
 
 private:
-    void updateGUID();
+    void initInternal_Win10(IServiceProvider* service);
+    void initInternal_Win11_21H2(IServiceProvider* service);
     void setFocus();
-
-    int isCurrentActive();
+    GUID getCurrDesktopGuid();
+    int  isCurrentActive();
 
     QVector<GUID> vd_guids;
     QVector<HWND> vd_win[6];
     QVector<IVirtualDesktop *> vd_desks;
+    QVector<IVirtualDesktop_Win11_21H2 *> vd_desks_win11_21H2;
     IVirtualDesktopManager         *pDesktopManager;
-    IVirtualDesktopManagerInternal *pDesktopManagerInt;
+    IVirtualDesktopManagerInternal *manager_int;
+    IVirtualDesktopManagerInternal_WIN11_21H2 *manager_int_win11_21H2;
+    int win_ver;
 };
 
 #endif // RE_WIN32_VIRT_H
